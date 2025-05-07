333300bc286a8deb245d00f4fe421d678245b4f4
Daniil Logunov <43185213+DanielELog@users.noreply.github.com>
2025-05-07 07:50:13 +0500

    wip

--- docker/buildClangir.sh
+++ docker/buildClangir.sh
@@ -54,7 +54,9 @@ function buildClangir() {
         -DCLANG_ENABLE_CIR=ON \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -GNinja ..
-  ninja -j16
+  ninja -j16 > /dev/null
+
+  echo "Finished compilation!"
 
   # <- clangir/llvm/build
   popd >/dev/null || exit 2
